2bd70403abe5aaf65ca50daea398f8bf7b2ad78d
Kaiser Perwez <KaiserPerwez@users.noreply.github.com>
2018-03-06 11:52:58 +0530

    Update Urls.java

--- app/src/main/java/com/wgt/hotsausagenew/utils/Urls.java
+++ app/src/main/java/com/wgt/hotsausagenew/utils/Urls.java
@@ -8,7 +8,7 @@ public class Urls {
     public static final String NETWORK_STATE_CHECK = "www.google.com";
     public static final String UPDATE_URL = "http://37.webmusic.pw/music/hindi/pop_remix/2018/s/single/320/Nazar-Lag-Jayegi_(webmusic.cc).mp3";
 
-    public static final String BASE_URL = "http://50.62.31.191/~sausage/new-sausage-apis/";
+    public static final String BASE_URL = "http://ip/~sausage/new-sausage-apis/";
     public static final String UPLOAD_TRANSACTION = BASE_URL + "saveTransaction.php";
     public static final String UPLOAD_SINGLE_USER = BASE_URL + "addNewUser.php";
     public static final String GET_ALL_USERS = BASE_URL + "getAllUsers.php";
